bc0f12752e4a28e2ec595e0554ac4fdc4053bbdd
Ahmed Anwar Gad <ahmed.anwar2003@gmail.com>
2025-06-09 01:11:05 +0300

    Update pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,31 +3,42 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    int p[2];              // File descriptors for the pipe
-    char recv_buf[5];     // Buffer to store received data (+1 for null terminator)
-    int status;            // Variable to hold exit status
+    int p1[2]; // Pipe: parent writes, child reads
+    int p2[2]; // Pipe: child writes, parent reads
+    char recv_buf[5]; // Buffer for received messages
+    int status;
 
-    pipe(p);              // Create a pipe
+    pipe(p1); // Create pipe for parent -> child
+    pipe(p2); // Create pipe for child -> parent
 
-    if (fork() == 0) {    // Child process
-        close(p[1]);      // Close the write end of the pipe in the child
-        read(p[0], recv_buf, 4);  // Read data from the pipe
-        recv_buf[4] = '\0'; // Null-terminate the string
-        close(p[0]);      // Close the read end of the pipe
-        printf("%d: received %s\n", getpid(), recv_buf); // Print the received message
-        write(p[1], "pong", 4);  // Write response back to the pipe
-        close(p[1]);      // Close the write end of the pipe
-        exit(0);          // Exit child process with status 0
-    } else {              // Parent process
-        close(p[0]);      // Close the read end of the pipe in the parent
-        write(p[1], "ping", 4);  // Write message to child
-        close(p[1]);      // Close the write end of the pipe
-        read(p[0], recv_buf, 4); // Read response from child
-        recv_buf[4] = '\0'; // Null-terminate the string
-        close(p[0]);      // Close the read end of the pipe
-        printf("%d: received %s\n", getpid(), recv_buf); // Print the received message
-        wait(&status);    // Wait for child process to finish and retrieve status
-        exit(0);          // Exit parent process with status 0
+    if (fork() == 0) {
+        // --- Child process ---
+        close(p1[1]); // Close unused write end of p1
+        read(p1[0], recv_buf, 4); // Read "ping"
+        recv_buf[4] = '\0';
+        close(p1[0]);
+
+        printf("%d: received %s\n", getpid(), recv_buf);
+
+        close(p2[0]); // Close unused read end of p2
+        write(p2[1], "pong", 4); // Send response
+        close(p2[1]);
+
+        exit(0);
+    } else {
+        // --- Parent process ---
+        close(p1[0]); // Close unused read end of p1
+        write(p1[1], "ping", 4); // Send message
+        close(p1[1]);
+
+        close(p2[1]); // Close unused write end of p2
+        read(p2[0], recv_buf, 4); // Read response
+        recv_buf[4] = '\0';
+        close(p2[0]);
+
+        printf("%d: received %s\n", getpid(), recv_buf);
+
+        wait(&status);
+        exit(0);
     }
 }
-
